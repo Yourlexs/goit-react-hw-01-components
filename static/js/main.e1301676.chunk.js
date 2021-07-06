c86f55235236e215b28d5a145f843bfc4b5aa489
Viktoria Alekseeva <Yourlexss@gmail.com>
2021-07-06 15:09:27 +0300

    Updates

--- static/js/main.e1301676.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{4:function(t,e,n){"use strict";n.r(e);var o=n(0),c=n.n(o),s=n(2),r=n.n(s),a=(n(9),n(3));r.a.render(Object(a.jsx)(c.a.StrictMode,{}),document.getElementById("root"))}},[[4,1,2]]]);
-//# sourceMappingURL=main.e1301676.chunk.js.map
